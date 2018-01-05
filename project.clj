ff737e9fb4f1b30396c64b53e51e849e0d56e34e
Stathis Sideris <stathis@pixelated-noise.com>
2018-01-05 04:09:19 +0200

    Added project.clj for compiling with Leiningen

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject stathissideris/ditaa "0.11.0"
+  :description "command-line utility that can convert diagrams drawn using ascii art into proper bitmap graphics"
+  :min-lein-version "2.0.0"
+  :url "https://github.com/stathissideris/ditaa"
+  :license {:name "GNU Lesser General Public License v3.0"
+            :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
+  :dependencies [[commons-cli/commons-cli "1.4"]
+                 [net.htmlparser.jericho/jericho-html "3.4"]
+                 [org.apache.xmlgraphics/batik-gvt "1.9"]
+                 [org.apache.xmlgraphics/batik-codec "1.9"]
+                 [org.apache.xmlgraphics/batik-bridge "1.9"]]
+  :main org.stathissideris.ascii2image.core.CommandLineConverter
+  :java-source-paths ["src"]
+  :profiles {:dev {:dependencies      [[junit/junit "4.12"]]
+                   :java-source-paths ["test"]}})
